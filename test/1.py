06cb3db0d31fdc583a532c9348414bef8909d83b
Kavindu_Methpura <kavindumethpura@gmail.com>
2025-01-12 19:49:52 +0530

    Delete 1.py

--- test/1.py
+++
@@ -1,9 +0,0 @@
-def fibonacci(n):
-    fib_series = [0, 1]
-    while len(fib_series) < n:
-        fib_series.append(fib_series[-1] + fib_series[-2])
-    return fib_series
-
-# Example usage
-n = 10  # Change this value to generate more or fewer Fibonacci numbers
-print(fibonacci(n))
